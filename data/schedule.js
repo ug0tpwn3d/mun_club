2bbbd62cc1b1e8da2348d9f20b98aca8e966a73b
SD <shivseeks@gmail.com>
2026-09-23 01:40:22 +0530

    feat: construct complete page architecture, unlinked nav routes, resources guides, and data layer

---
+++ data/schedule.js
@@ -0,0 +1,8 @@
+export const scheduleData = [
+    { time: "08:30 - 09:30", event: "Delegate Registration & Kit Distribution" },
+    { time: "10:00 - 11:00", event: "Opening Ceremony & Executive Board Introduction" },
+    { time: "11:30 - 13:30", event: "Committee Session I (Agenda Discussion & GSL)" },
+    { time: "13:30 - 14:30", event: "Lunch & Informal Diplomatic Consultations" },
+    { time: "14:30 - 17:00", event: "Committee Session II (Crisis Directives & Resolution Voting)" },
+    { time: "17:30 - 18:30", event: "Valedictory Ceremony & Best Delegation Awards" }
+];
