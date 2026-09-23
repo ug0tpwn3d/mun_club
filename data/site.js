b9802f293ed28a4f0cbcfbfa2dbff7d9136fa40c
SD <shivseeks@gmail.com>
2026-09-23 06:13:54 +0530

    Replace global style.css and script.js with page-specific css and js across all pages; migrate data/ to js/pages/

--- data/site.js
+++
@@ -1,9 +0,0 @@
-export const siteData = {
-    name: "SKIT MUN CLUB",
-    institute: "Swami Keshvanand Institute of Technology, Management & Gramothan, Jaipur",
-    email: "skitmg.mun.club@gmail.com",
-    instagram: "https://www.instagram.com/skit.mun.club",
-    established: 2020,
-    flagshipConference: "ARAMBH MUN",
-    location: "Jaipur, Rajasthan, India"
-};
